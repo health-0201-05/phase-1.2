469bcc891f69d0a55d2b908d1ea61f8416c63c24
Nadula Kadawedduwa <53409084+NadulaKadawedduwa@users.noreply.github.com>
2024-02-24 22:44:22 -0500

    Update t4.js
    added comments explaining choice to use .display instead of .visibility

--- js/t4.js
+++ js/t4.js
@@ -1,3 +1,5 @@
+// Simply sets the visiblity of the popup container to
+// none or block depending on whether it is currently visible
 function openPopup() {
     var x = document.getElementById("popupContainer");
     if (x.style.display != "block") {
@@ -5,6 +7,8 @@ function openPopup() {
     }
 }
 
+// .style.visibility did not want to work with the flex
+// display so setting .style.display is the work around
 function closePopup() {
     var x = document.getElementById("popupContainer");
     if (x.style.display != "none") {
